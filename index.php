0805fb34b1cd8354b31ec537e0644325a125cb8e
William Harrison <william@williamdavidharrison.com.au>
2023-04-10 14:47:47 +0800

    feat: fix add item bug + add custom bullet point

--- index.php
+++ index.php
@@ -38,16 +38,16 @@ if(isset($_GET["remove"])) { // Check if remove parameter is set in the URL
 		<div class="container">
 			<h1>My To-Do List</h1>
 
-			<form method="POST">
+			<form action="/" method="POST">
 				<input type="text" name="task" placeholder="Enter task..." minlength="1" maxlength="64" required>
 				<button type="submit">Add</button>
 			</form>
 
 			<?php foreach ($tasks as $index => $task): ?>
 				<p>
-					<span><?php echo $index + 1; ?>.</span>
+					<span></span>
 					<?php echo $task; ?>
-					<a type="submit" class="remove" href="?remove=<?php echo $index; ?>">Remove</a>
+					<a type="submit" class="remove" href="/?remove=<?php echo $index; ?>">Remove</a>
 				</p>
 			<?php endforeach; ?>
 		</div>
